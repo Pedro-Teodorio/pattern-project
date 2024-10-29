e327f9e2c79c1378ad28e8ea6906459c8946a330
Pedro-Teodorio <pedroteodorio92@gmail.com>
2024-10-29 12:46:08 -0300

    Update package.json and Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { Input } from "./components/Input";
+import { useForm } from "react-hook-form";
+
 
 export default function App() {
-	return (
-		<div className="w-full bg-neutral-800 space-y-4 p-6">
-			<Input.Root width="w-80" height="h-12">
-				<Input.Icon name="UserRound" />
-				<Input.Content placeholder="Nome Completo" type="text" name="nome" />
-			</Input.Root>
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
-			<Input.Root width="w-80" height="h-10" mode="dark">
-				<Input.Content placeholder="Search" type="text" name="search" />
-				<Input.Icon name="Search" />
-			</Input.Root>
+	const onSubmit = (data) => console.log(data);
+	return (
+		<div className=" mx-auto w-full bg-neutral-800 space-y-4 p-6">
+			<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+				<Input.Root width="w-80" height="h-10">
+					<Input.Icon name="UserRound" />
+					<Input.Content placeholder="Nome Completo" type="text" register={{ ...register("name", { required: true }) }} />
+				</Input.Root>
+				{errors.name && <span className="text-red-500">Campo nome é obrigatório</span>}
+
+				<Input.Root width="w-80" height="h-10" mode="dark">
+					<Input.Icon name="IdCard" />
+					<Input.Content placeholder="CPF" type="text" register={{ ...register("cpf") }} />
+				</Input.Root>
+
+				<button type="submit" className=" w-80 p-2 bg-emerald-500 text-neutral-50 rounded-lg ">
+					Enviar
+				</button>
+			</form>
 		</div>
 	);
 }
--- src/components/Input/Content/index.jsx
+++ src/components/Input/Content/index.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
-export const InputContent = ({ placeholder, type, name }) => {
-	return <input type={type} name={name} placeholder={placeholder} className={`w-full px-2 bg-transparent outline-none  h-full placeholder:text-neutral-400`} />;
+export const InputContent = ({ placeholder, type, register }) => {
+	return <input type={type} placeholder={placeholder} className={`w-full px-2 bg-transparent outline-none  h-full placeholder:text-neutral-400`} {...register} />;
 };
 
 InputContent.propTypes = {
 	placeholder: PropTypes.string.isRequired,
 	type: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
+	register: PropTypes.object,
 };
